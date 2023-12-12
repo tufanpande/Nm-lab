a11c5913d675abece5db947750174dbe85ecd4ef
Tufan Pandey <99518915+tufanpande@users.noreply.github.com>
2023-12-12 16:48:30 +0545

    Add files via upload

---
+++ falsePositionMethod.c
@@ -0,0 +1,38 @@
+//Implemtation using False Position method
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float f(float x){
+	return x*x*x-4*x-9;
+}
+
+
+
+int main(){
+	float a,b,c,e=0.0001;
+	printf("Enter the initial guess a and b:");
+	scanf("%f%f",&a,&b);
+	if (f(a)*f(b)>0){
+		printf("our initial guess is wrong");
+	}	
+	else{
+		do{
+			c=a-(b-a)*f(a)/(f(b)-f(a));
+			if (f(a)*f(c)>0){
+				a=c;
+				
+			}
+			else{
+				b=c;
+				
+			}
+			 printf("%f\t%f\t%f\t%f\t%f\t\n",a,b,f(a),f(b),c);
+		}
+		while(fabs (f(c))>e);
+		
+	}
+	printf("The root of equation is:%f",c);
+	
+	return 0;
+	
+}
